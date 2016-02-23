f9a83f23160b70ea02251a7f25f083c09ca8fef3
ScottSolomon66 <scott.solomon66@gmail.com>
2016-02-23 17:16:15 -0600

    Method function/S4 functions
    I wrote a generic method for play_game. Also, I created a function that
    would take an object and turn it into a door. Also I wrote a function
    that would take an integer, turn it into class door, then execute the
    play_game function

--- Problem-Set-3.R
+++ Problem-Set-3.R
@@ -11,16 +11,23 @@ door<-structure(vector,class = "door", length = 1, range = 1:3)
 random_door<-(sample(1,1:3)) #just making a random integer from 1 to 3
 class(random_door)<-"door" #changing the class to door
 
-##door.play_game function
+
+##play_game function
+##this is creating a generic method for play_game
+play_game<-function(x){
+  UseMethod("play_game", x)
+}
+
+##play_game.door function
 ##this method takes in an object of class door, and returns a message if it is not the right class
 ##the function creates a random number 1 to 3, to represent which door has the car behind it
 ##it either returns the message that the person selected the goat or the car
 ##it is decides which message to give based off of the object inputed matches the random number generated
 
-door.play_game<-function(x, class = "door"){ #using door.to make a method for the class
+play_game.door<-function(x, class = "door"){ #using .door to make a method for the class
   ##this if statement returns a message about whether the object is a door
   if (class(x) != "door"){ 
-    message<-list("This is not of object class door")
+    message<-("This is not of object class door")
     ##the code will end below if the object is not of class door
     return(message)
   }
@@ -39,14 +46,40 @@ door.play_game<-function(x, class = "door"){ #using door.to make a method for th
   }
 }
 
-##choose_door
+##random_door_selector
 ##this function basically chooses a door for you
 ##it ensures the random integer is in the class "door"
 
-choose_door<-function(x){
-  random_int<-sample(1:3,1)
-  class(random_int)<-"door"
+random_door_selector<-function(x){
+  random_int<-sample(1:3,1) #gives a random integer from 1 to 3
+  class(random_int)<-"door" #assigns it to class door
   return(random_int)
 }
 
+##this line will choose a random door for you and play the game
+##good luck
+play_game.door(random_door_selector())
 
+
+##this function will take an integer from 1 to 3 and make it class door
+door_creator<-function(x){
+  if (x>3 | x<1){
+    out_of_range_message<-"This number is not between 1 and 3"
+    return(out_of_range_message)
+  }
+  if (length(x)!=1){
+    wrong_length_message<-"The object must be of length 1"
+    return(wrong_length_message)
+  }
+  if (x%%1 != 0){
+    not_an_integer_message<-"This object is not an integer"
+    return(not_an_integer_message)
+  }
+  class(x)<-"door"
+  return(x)
+}
+
+play_game_input_non_door<-function(x){
+  new_door<-door_creator(x)
+  play_game.door(new_door)
+}
